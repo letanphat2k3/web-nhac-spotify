72d261111de36e3b2a7b270d4124e1cf100010d5
letanphat2k3 <ltpht24022k3@gmail.com>
2024-11-03 14:08:22 +0700

    Feat:upload_code_secon1

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ export default function App() {
   const [{ token }, dispatch] = useStateProvider();
 
   useEffect(() => {
+    // Lấy token từ localStorage
     let token = localStorage.getItem("token");
 
+    // Nếu token không tồn tại trong localStorage, kiểm tra URL
     if (!token) {
       const hash = window.location.hash;
       if (hash) {
         token = hash.split("&")[0].split("=")[1];
         localStorage.setItem("token", token); // Lưu token vào localStorage
-        window.location.hash = "";
+        window.location.hash = ""; // Xóa hash từ URL
       }
     }
 
+    // Nếu tìm thấy token, cập nhật state với token đó
     if (token) {
       dispatch({ type: reducerCases.SET_TOKEN, token });
     }
   }, [dispatch]);
 
+  // Hiển thị giao diện dựa trên trạng thái token
   return <div>{token ? <Spotify /> : <Login />}</div>;
 }
