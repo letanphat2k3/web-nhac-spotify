6c4c3cf0d90c24e73ffa227d031f63fedd61bd05
letanphat2k3 <ltpht24022k3@gmail.com>
2024-11-03 14:02:41 +0700

    Feat:upload_code_secon

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { reducerCases } from "./action/Constants";
 import Spotify from "./components/Spotify/Spotify";
 import Login from "./components/Login/Login";
 
-
 export default function App() {
   const [{ token }, dispatch] = useStateProvider();
-  
-  useEffect(() => {
-    const hash = window.location.hash;
-    console.log("URL Hash:", hash);  
-    if (hash) {
-      const token = hash.split("&")[0].split("=")[1];
-      dispatch({ type: reducerCases.SET_TOKEN, token });
-      window.location.hash = ""; 
-    }
-  }, [dispatch])
 
-  return (<div>{token ? <Spotify /> : <Login />}</div>);
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+
+    if (!token) {
+      const hash = window.location.hash;
+      if (hash) {
+        token = hash.split("&")[0].split("=")[1];
+        localStorage.setItem("token", token); // Lưu token vào localStorage
+        window.location.hash = "";
+      }
+    }
+
+    if (token) {
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [dispatch]);
+
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -13,80 +13,79 @@ import { FiRepeat } from 'react-icons/fi';
 
 const PlayerControls = () => {
     const [{ token, playerState }, dispatch] = useStateProvider();
-    const changeTrack = async (type) => {
-        await axios.post(
-          `https://api.spotify.com/v1/me/player/${type}`,
-            {},
-            {
-                headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + token,
-                },
-            }
-        );
-        dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
-        const response1 = await axios.get(
-            "https://api.spotify.com/v1/me/player/currently-playing",
-            {
-                headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + token,
-                },
-            }
-        );
-        if (response1.data !== "") {
-            const currentPlaying = {
-                id: response1.data.item.id,
-                name: response1.data.item.name,
-                artists: response1.data.item.artists.map((artist) => artist.name),
-                image: response1.data.item.album.images[2].url,
-            };
-            dispatch({ type: reducerCases.SET_PLAYING, currentPlaying });
-        } else {
-            dispatch({ type: reducerCases.SET_PLAYING, currentPlaying: null });
-        }
-    };
 
-    const changeState = async () => {
-        const state = playerState ? "pause" : "play";
-        await axios.put(
-            `https://api.spotify.com/v1/me/player/${state}`,
-            {},
-                {
-                    headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + token,
-                    },
-                }
-            );
-            dispatch({
-                type: reducerCases.SET_PLAYER_STATE,
-                playerState: !playerState,
-            });
+  const changeState = async () => {
+    const state = playerState ? "pause" : "play";
+    await axios.put(
+      `https://api.spotify.com/v1/me/player/${state}`,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+    dispatch({
+      type: reducerCases.SET_PLAYER_STATE,
+      playerState: !playerState,
+    });
+  };
+  const changeTrack = async (type) => {
+    await axios.post(
+      `https://api.spotify.com/v1/me/player/${type}`,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+    dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
+    const response1 = await axios.get(
+      "https://api.spotify.com/v1/me/player/currently-playing",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+    if (response1.data !== "") {
+      const currentPlaying = {
+        id: response1.data.item.id,
+        name: response1.data.item.name,
+        artists: response1.data.item.artists.map((artist) => artist.name),
+        image: response1.data.item.album.images[2].url,
       };
-  
+      dispatch({ type: reducerCases.SET_PLAYING, currentPlaying });
+    } else {
+      dispatch({ type: reducerCases.SET_PLAYING, currentPlaying: null });
+    }
+  };
   return (
     <Container>
-    <div className="shuffle">
-      <BsShuffle />
-    </div>
-    <div className="previous">
-      <CgPlayTrackPrev onClick={() => changeTrack("previous")} />
-    </div>
-    <div className="state">
-      {playerState ? (
-        <BsFillPauseCircleFill onClick={changeState} />
-      ) : (
-        <BsFillPlayCircleFill onClick={changeState} />
-      )}
-    </div>
-    <div className="next">
-      <CgPlayTrackNext onClick={() => changeTrack("next")} />
-    </div>
-    <div className="repeat">
-      <FiRepeat />
-    </div>
-  </Container>
+      <div className="shuffle">
+        <BsShuffle />
+      </div>
+      <div className="previous">
+        <CgPlayTrackPrev onClick={() => changeTrack("previous")} />
+      </div>
+      <div className="state">
+        {playerState ? (
+          <BsFillPauseCircleFill onClick={changeState} />
+        ) : (
+          <BsFillPlayCircleFill onClick={changeState} />
+        )}
+      </div>
+      <div className="next">
+        <CgPlayTrackNext onClick={() => changeTrack("next")} />
+      </div>
+      <div className="repeat">
+        <FiRepeat />
+      </div>
+    </Container>
   );
 };
 
